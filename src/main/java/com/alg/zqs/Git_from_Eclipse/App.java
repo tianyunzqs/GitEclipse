1702d08e3f285874972f265c73b13db631072a91
lenovo <lenovo@DESKTOP-0MGSQ9B>
2017-07-02 23:46:42 +0800

    这是从eclipse提交的maven项目

---
+++ src/main/java/com/alg/zqs/Git_from_Eclipse/App.java
@@ -0,0 +1,14 @@
+package com.alg.zqs.Git_from_Eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "这是一个演示如何从eclipse创建maven项目并上传到GitHub的演示项目" );
+    }
+}
